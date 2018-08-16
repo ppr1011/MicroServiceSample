cae70bba65d0c386fb580a269d7d89a51513426d
ppr1011 <809277417@qq.com>
2018-08-16 16:11:27 +0800

    服务提供者
    服务提供者

---
+++ providers/src/main/java/com/exercise/providers/dto/User.java
@@ -0,0 +1,22 @@
+package com.exercise.providers.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+	private String name;
+	private Integer age;
+	
+	public User() {
+		
+	}
+	
+	public User(String name,Integer age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String toString(){
+		return "name=" + name + ",age="+age;
+	}
+}
